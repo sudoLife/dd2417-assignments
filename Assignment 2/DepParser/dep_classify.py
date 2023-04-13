b9bdcd32d4050f3d28f5dbdb48799cb3605ec9b6
sudoLife <sudoLife1@proton.me>
2023-04-13 15:31:58 +0200

    Basic classify

--- Assignment 2/DepParser/dep_classify.py
+++ Assignment 2/DepParser/dep_classify.py
@@ -2,7 +2,8 @@ import os
 import pickle
 from parse_dataset import Dataset
 from dep_parser import Parser
-from DepParser.logreg_my import LogisticRegression
+from logreg import LogisticRegression
+import numpy as np
 
 
 class TreeConstructor:
@@ -10,7 +11,7 @@ class TreeConstructor:
     This class builds dependency trees and evaluates using unlabeled arc score (UAS) and sentence-level accuracy
     """
 
-    def __init__(self, parser):
+    def __init__(self, parser: Parser):
         self.__parser = parser
 
     def build(self, model, words, tags, ds):
@@ -26,7 +27,33 @@ class TreeConstructor:
         #
         # YOUR CODE HERE
         #
-        pass
+        # In the 'build' method, you need to take the current configuration (stack,
+        # buffer, tree), and turn that into a data point from which the model can
+        # predict the next action. Then, you perform that action, get a new
+        # configuration, turn that into a data point, predict the next action, and so
+        # on. Keep going until you reach an end state with an empty buffer and a stack
+        # containing only ROOT.
+        parser = self.__parser
+
+        i, stack, pred_tree = 0, [], [0] * len(words)
+
+        while i < len(pred_tree) or len(stack) > 1:
+            # get the prediction and move stuff
+            datapoint = ds.dp2array(words, tags, i, stack)
+            # expand the dims
+            prediction = model.predict(np.concatenate((datapoint, [1.0])))
+            moves = np.argsort(prediction)[::-1]
+
+            valid_moves = parser.valid_moves(i, stack, pred_tree)
+            selected_move = -1
+            for move in moves:
+                if move in valid_moves:
+                    selected_move = move
+                    break
+            # NOTE: I'm assuming at least one move is valid?
+            i, stack, pred_tree = parser.move(
+                i, stack, pred_tree, selected_move)
+        return pred_tree
 
     def evaluate(self, model, test_file, ds):
         """
@@ -36,40 +63,58 @@ class TreeConstructor:
         :param      test_file:  The CONLL-U test file
         :param      ds:         Training dataset instance having the feature maps
         """
-        #
+
         # YOUR CODE HERE
-        #
-        pass
+
+        parser = self.__parser
+        # test_ds = parser.create_dataset(test_file)
+        correct_sentences = 0
+        total_sentences = 0
+        with open(test_file, 'r') as source:
+            for words, tags, tree, relations in parser.trees(source):
+                # call the build function
+                total_sentences += 1
+                moves = self.build(model, words, tags, ds)
+                correct_sentences += moves == tree
+
+            print(
+                f'Sentence-level accuracy: {correct_sentences / total_sentences}')
 
 
-if __name__ == '__main__':
+train_set_path = "/home/sudolife/Documents/KTH/Language Engineering/Assignment 2/DepParser/en-ud-train-projective.conllu"
+dev_set_path = "/home/sudolife/Documents/KTH/Language Engineering/Assignment 2/DepParser/en-ud-dev-projective.conllu"
+model_path = "/home/sudolife/Documents/KTH/Language Engineering/Assignment 2/DepParser/model.pkl"
 
+
+def main():
     # Create parser
     p = Parser()
 
     # Create training dataset
-    ds = p.create_dataset(
-        "/home/sudolife/Documents/KTH/Language Engineering/Assignment 2/DepParser/en-ud-train-projective.conllu", train=True)
+    ds = p.create_dataset(train_set_path, train=True)
 
     # Train LR model
-    if os.path.exists('model.pkl'):
+    if os.path.exists(model_path):
         # if model exists, load from file
         print("Loading existing model...")
-        lr = pickle.load(open('model.pkl', 'rb'))
+        mlr = pickle.load(open(model_path, 'rb'))
     else:
         # train model using minibatch GD
-        lr = LogisticRegression()
-        lr.fit(*ds.to_arrays())
-        pickle.dump(lr, open('model.pkl', 'wb'))
+        mlr = LogisticRegression()
+        mlr.fit(*ds.to_arrays())
+        pickle.dump(mlr, open(model_path, 'wb'))
 
     # Create test dataset
-    test_ds = p.create_dataset(
-        "/home/sudolife/Documents/KTH/Language Engineering/Assignment 2/DepParser/en-ud-dev-projective.conllu")
+    test_ds = p.create_dataset(dev_set_path, train=False)
     # Copy feature maps to ensure that test datapoints are encoded in the same way
     test_ds.copy_feature_maps(ds)
     # Compute move-level accuracy
-    lr.classify_datapoints(*test_ds.to_arrays())
+    mlr.classify_datapoints(*test_ds.to_arrays())
 
     # Compute UAS and sentence-level accuracy
     t = TreeConstructor(p)
-    t.evaluate(lr, 'en-ud-dev-projective.conllu', ds)
+    t.evaluate(mlr, dev_set_path, ds)
+
+
+if __name__ == '__main__':
+    main()
